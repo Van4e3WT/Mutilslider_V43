f90adbaa8aba427e980267c152a8847d4d15090f
IvanG <van4e3wt@yandex.ru>
2021-01-12 17:49:13 +0500

    Extended plugin solo/double type and added events

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,34 @@
 import './style.scss';
 import './init/mainJQuery.ts';
 
+$('.multislider-v43.slider-1').multislider({
+  minValue: 5000,
+  maxValue: 15000,
+  step: 100,
+  value: 5440,
+
+  orientation: 'vertical',
+  sliderType: 'double',
+
+  popUpOfValue: true,
+  scaleOfValues: true,
+  isProgressBar: true,
+
+  description: 'Range slider',
+});
 $('.multislider-v43').each(function(i) {
   $(this).multislider({
     minValue: 0,
     maxValue: 1000,
     step: 10,
+    value: 544,
   
     orientation: 'horizontal',
     sliderType: 'solo',
   
-    popUpOfValue: true,
-    scaleOfValues: true,
-    isProgressBar: true,
+    popUpOfValue: false,
+    scaleOfValues: false,
+    isProgressBar: false,
   
     description: "Мой слайдер " + i,
   });
--- src/init/mainJQuery.ts
+++ src/init/mainJQuery.ts
@@ -12,6 +12,7 @@
     minValue: number,
     maxValue: number,
     step: number,
+    value?: number,
 
     orientation: 'vertical' | 'horizontal',
     sliderType: 'solo' | 'double',
@@ -20,7 +21,7 @@
     scaleOfValues: boolean,
     isProgressBar: boolean,
 
-    description: string
+    description?: string
   }
 
   $.fn.multislider = function(cfg: Config = {
@@ -36,9 +37,13 @@
     scaleOfValues: true,
     isProgressBar: true,
 
-    description: "Range Slider"
+    description: 'Range Slider'
   }) {
+
     const el: HTMLDivElement = this[0];
+    if(this.length === 0)
+      throw new Error('Not found element for initialization');
+
     //==================== Отрисовка блоков и слайдера ====================
 
     const sliderDescription = document.createElement('div');
@@ -54,19 +59,87 @@
     sliderDescription.appendChild(sliderOutput);
     el.appendChild(sliderDescription);
 
-    const sliderAlone = document.createElement('input');
-    sliderAlone.min = String(cfg.minValue);
-    sliderAlone.max = String(cfg.maxValue);
-    sliderAlone.step = String(cfg.step);
-    sliderAlone.value = '544';
-    sliderAlone.classList.add('multislider-v43__input');
-    sliderAlone.type = 'range';
-
     const slidersInput = document.createElement('div');
     slidersInput.classList.add('multislider-v43__slider');
-    slidersInput.appendChild(sliderAlone);
+
+    let sliders: Array<HTMLInputElement> = [];
+
+    if(cfg.sliderType === 'double') {
+
+      const sliderLeft = document.createElement('input');
+      const sliderRight = document.createElement('input');
+
+      sliderLeft.type = 'range';
+      sliderLeft.classList.add('multislider-v43__input');
+      sliderLeft.min = String(cfg.minValue);
+      sliderLeft.max = String(cfg.maxValue);
+      sliderLeft.value = String(cfg.step * Math.floor((cfg.maxValue - cfg.minValue) / 100 / 3) + cfg.minValue);
+      sliderLeft.step = String(cfg.step);
+
+      sliderRight.type = 'range';
+      sliderRight.classList.add('multislider-v43__input');
+      sliderRight.min = String(cfg.minValue);
+      sliderRight.max = String(cfg.maxValue);
+      sliderRight.value = String(cfg.step * Math.floor((cfg.maxValue - cfg.minValue) / 100 / 3 * 2) + cfg.minValue);
+      sliderRight.step = String(cfg.step);
+
+      sliderOutput.innerText = sliderLeft.value + ' - ' + sliderRight.value;
+
+      sliders.push(sliderLeft);
+      sliders.push(sliderRight);
+
+    } else {
+
+      const sliderAlone = document.createElement('input');
+      sliderAlone.min = String(cfg.minValue);
+      sliderAlone.max = String(cfg.maxValue);
+      sliderAlone.step = String(cfg.step);
+      sliderAlone.value = String(cfg.value || 0);
+      sliderAlone.classList.add('multislider-v43__input');
+      sliderAlone.type = 'range';
+      sliderOutput.innerText = sliderAlone.value;
+  
+      sliders.push(sliderAlone);
+
+    }
+
+    sliders.forEach( (item) => {slidersInput.appendChild(item)});
     el.appendChild(slidersInput);
 
+    //=========================================================================
+
+    //=========== Доступ к значению слайдера через свойство функции ===========
+
+    $.fn.multislider.setValue = function(value: number, valueExtended?: number) { // вот здесь надо поправить, чтобы всегда мин присваивалось к левому, макс к правому
+      sliders[0].value = String(value);
+      if(sliders.length > 1) sliders[1].value = String(valueExtended);
+    };
+    $.fn.multislider.getValue = function() {
+      return sliders;
+    }
+
+    //=========================================================================
+
+    //========================= Обработка событий =============================
+    function outputGenerating() {
+      let result: string = sliders[0].value;
+      for(let i = 1; i < sliders.length; ++i) {
+        result += ' - ' + sliders[i].value;
+      }
+
+      sliderOutput.innerText = result;
+    }
+
+    if(cfg.sliderType === 'double') {
+      sliders[0].addEventListener('input', () => {sliders[0].value = String(Math.min(+sliders[1].value, +sliders[0].value));});
+      sliders[1].addEventListener('input', () => {sliders[1].value = String(Math.max(+sliders[0].value, +sliders[1].value));});
+    }
+
+    sliders.forEach( (item) => {
+      item.addEventListener('input', () => {
+        outputGenerating();
+      });
+    });
     //=========================================================================
   };
 })(jQuery);
