d04d8df216fa6cea667a530be156926fa498323e
IvanG <van4e3wt@yandex.ru>
2021-02-11 15:00:49 +0500

    Added few tests UI

--- tests/view.test.ts
+++ tests/view.test.ts
@@ -7,6 +7,8 @@ describe('***VIEW***', () => {
     let browser: any;
     let page: any;
 
+    const selector: string = '.multislider-v43';
+
     beforeEach(async () => {
       browser = await puppeteer.launch({
         headless: true,
@@ -34,11 +36,7 @@ describe('***VIEW***', () => {
       await browser.close();
     });
 
-    test('should build foundation DOM struct', async () => {
-      const selector: string = '.multislider-v43';
-
-      // await page.waitForSelector(selector);
-
+    test('should build foundation DOM struct', async () => { // переделать этот тест под проверку всех соло слайдеров, как в тесте ниже
       const sliderParent = await page.$(`${selector}.solo`);
 
       expect(sliderParent).not.toBeUndefined();
@@ -53,6 +51,30 @@ describe('***VIEW***', () => {
 
       expect(sliderBody).not.toBeUndefined();
       expect(sliderBody).not.toBeNull();
+    });
+
+    test('should be contain just one thumb slider', async () => { // проверить, как это будет работать на двухдиапазонном слайдере, если что, то вернуться и переделать
+      const sliders = await page.evaluate((sel: Element) => {
+        const arr: Array<Array<Element>> = [];
+        const elems = Array.from(document.querySelectorAll(`${sel}.solo`));
+
+        elems.forEach((elem) => {
+          const sliderThumb = Array.from(elem.querySelectorAll(`${sel}-body__thumb`));
+          arr.push(sliderThumb);
+        });
+        return arr;
+      }, selector);
+
+      sliders.forEach((slider: Array<Element>) => {
+        expect(slider.length).toBe(1);
+      });
+    });
+
+    test('should be rendered scale if it\'s enabled', async () => { // здесь конкретный экземпляр для проверки теста, всё ок
+      const sliderParent = await page.$(`${selector}.solo.slider-4`);
+      const scaleDivisions = await sliderParent.$$(`${selector}-body__scale-division`);
+
+      expect(scaleDivisions.length).toBeGreaterThan(0);
     });
   });
 });
