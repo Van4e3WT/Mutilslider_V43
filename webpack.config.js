c899e21191540f82222ffc7e8e38ed1337c2e7f6
IvanG <van4e3wt@yandex.ru>
2021-04-20 16:59:41 +0500

    Added PUG and updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
@@ -7,8 +8,19 @@ module.exports = {
     'multislider-v43': './src/multislider-v43/index.ts',
     example: './src/example/index.ts',
   },
+  output: {
+    filename: '[name]/[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
   module: {
     rules: [
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        options: {
+          pretty: true,
+        },
+      },
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -27,13 +39,12 @@ module.exports = {
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
-  output: {
-    filename: '[name]/[name].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
   plugins: [
-    // при добавлении HtmlWebpackPlugin добавить chunks область видимости, чтобы в файл подключались
-    // только нужные скрипты и стили (если знач по умолчанию не удовлетворяет)
+    new HtmlWebpackPlugin({
+      template: './src/example/index.pug',
+      filename: 'index.html',
+      inject: 'body',
+    }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: '[name]/[name].css',
