88e08b90f25e2d78704f61eac78308d3b681ac37
IvanG <van4e3wt@yandex.ru>
2021-06-17 12:28:50 +0500

    fix example lint

--- src/example/pages/index/index.ts
+++ src/example/pages/index/index.ts
@@ -1,6 +1,6 @@
 /* global $ */
-import '../../blocks/configurable-slider/configurable-slider-init';
 import './index.scss';
+import '../../blocks/configurable-slider/configurable-slider-init';
 
 $('.js-multislider-v43_double.js-multislider-v43_slider-1').multislider({
   minValue: 5000,
