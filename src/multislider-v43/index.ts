a841e1d7b26e4bca9c2e65d17f35105599504a13
IvanG <ivan.gladyshev@mail.ru>
2021-09-06 17:21:39 +0500

    Update attribute-init params

--- src/multislider-v43/index.ts
+++ src/multislider-v43/index.ts
@@ -25,12 +25,12 @@ $elems.each((i, elem) => {
   if (elem.hasAttribute('data-step')) props.step = Number(elem.getAttribute('data-step'));
   if (elem.hasAttribute('data-value1')) props.value1 = Number(elem.getAttribute('data-value1'));
   if (elem.hasAttribute('data-value2')) props.value2 = Number(elem.getAttribute('data-value2'));
-  if (elem.hasAttribute('data-is-vertical')) props.isVertical = (elem.getAttribute('data-is-vertical') === 'true');
-  if (elem.hasAttribute('data-is-range')) props.isRange = (elem.getAttribute('data-is-range') === 'true');
-  if (elem.hasAttribute('data-tooltip-of-value')) props.tooltipOfValue = (elem.getAttribute('data-tooltip-of-value') === 'true');
+  if (elem.hasAttribute('data-is-vertical')) props.isVertical = true;
+  if (elem.hasAttribute('data-is-range')) props.isRange = true;
+  if (elem.hasAttribute('data-tooltip-of-value')) props.tooltipOfValue = true;
   if (elem.hasAttribute('data-scale-of-values')) props.scaleOfValues = Number(elem.getAttribute('data-scale-of-values'));
-  if (elem.hasAttribute('data-is-progress-bar')) props.isProgressBar = (elem.getAttribute('data-is-progress-bar') === 'true');
-  if (elem.hasAttribute('data-tooltip-is-hidden')) props.tooltipIsHidden = (elem.getAttribute('data-tooltip-is-hidden') === 'true');
+  if (elem.hasAttribute('data-is-progress-bar')) props.isProgressBar = true;
+  if (elem.hasAttribute('data-tooltip-is-hidden')) props.tooltipIsHidden = true;
   if (elem.hasAttribute('data-description')) props.description = elem.getAttribute('data-description');
   if (elem.hasAttribute('data-postfix')) props.postfix = elem.getAttribute('data-postfix');
 
