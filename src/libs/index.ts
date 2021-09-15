8f3a7c682a7003a1cd30759fd49146b60ba9c735
IvanG <ivan.gladyshev@mail.ru>
2021-09-15 11:18:26 +0500

    Fix issue #14 hotfix nodeJS global

--- src/libs/index.ts
+++ src/libs/index.ts
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 
-const globalAny: NodeJS.Global & typeof globalThis & { jQuery?: unknown, $?: unknown } = global;
+const globalAny: typeof globalThis & { jQuery?: unknown, $?: unknown } = global;
 
 globalAny.jQuery = $;
 globalAny.$ = $;
